dc34abd7660ddbab7d84681d1a34059aad23a4c1
Jinkyu <xxyxy000@gmail.com>
2022-06-16 21:47:31 +0900

    Style home

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,12 +1,12 @@
 import styles from "./Circle.module.css";
 
-const Circle = () => {
+const Circle = ({ type }) => {
   return (
     <div className={styles.Circle}>
-      <div className={styles.circleElem}></div>
-      <div className={styles.circleElem}></div>
-      <div className={styles.circleElem}></div>
-      <div className={styles.circleElem}></div>
+      <div className={[styles.circleElem, styles[type]].join(" ")}></div>
+      <div className={[styles.circleElem, styles[type]].join(" ")}></div>
+      <div className={[styles.circleElem, styles[type]].join(" ")}></div>
+      <div className={[styles.circleElem, styles[type]].join(" ")}></div>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
+import Circle from "../components/Circle.js";
 import styles from "./Home.module.css";
 
 const Home = () => {
   return (
     <div className={styles.Home}>
       <div className={styles.homeWrap}>
-        <h1>숨만 쉬어도<br />나가는 돈<br />얼마일까?
-          <figure className={styles.homeImg}>
-            <img src={process.env.PUBLIC_URL + `/assets/home.png`} srcSet={process.env.PUBLIC_URL + `/assets/home@2x.png`} alt="money" />
-          </figure>
-        </h1>
+        <h1>숨만 쉬어도<br />나가는 돈</h1>
+        <div className={styles.circleWrap}>
+          <Circle type="white" />
+        </div>
         <p>고정 지출비 설정하고</p>
         <Link to="/edit">
-        <div className={styles.enterBtn}><span>확인하기</span><span className={styles.next}></span></div>
+        <div className={styles.enterBtn}><span>얼마인지 확인하기</span><span className={styles.next}></span></div>
         </Link>
       </div>
     </div>
